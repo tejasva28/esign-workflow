5d34fdd3932f1574b0bf16a10ec6e3c7518e768b
tejasva28 <32809735+tejasva28@users.noreply.github.com>
2024-07-01 14:30:53 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
-import { PDFController } from './infrastructure/controllers/pdf.controller';
-import { pdfProviders } from './infrastructure/providers/pdf.providers';
+const { ConfigModule, ConfigService } = require('@nestjs/config');
+const { HttpModule, HttpService } = require('@nestjs/axios');
+const pdfControllerFactory = require('./infrastructure/controllers/pdf.controller');
+const SignPDFUseCase = require('./application/use_cases/sign-pdf.usecase');
+const PDFService = require('./domain/services/pdf.service');
+const OpenSignService = require('./domain/services/opensign.service');
+const PDFRepositoryImpl = require('./infrastructure/repositories/pdf.repository.impl');
+const express = require('express');
 
-@Module({
-  imports: [],
-  controllers: [PDFController],
-  providers: [...pdfProviders],
-})
-export class AppModule {}
+class AppModule {
+  static imports = [
+    ConfigModule.forRoot(),
+    HttpModule,
+  ];
+
+  static providers = [
+    SignPDFUseCase,
+    {
+      provide: 'PDFRepository',
+      useClass: PDFRepositoryImpl,
+    },
+    {
+      provide: PDFService,
+      useFactory: (configService, httpService, pdfRepository) => new PDFService(configService, httpService, pdfRepository),
+      inject: [ConfigService, HttpService, 'PDFRepository'],
+    },
+    OpenSignService,
+  ];
+
+  configureRoutes(app) {
+    const configService = new ConfigService();
+    const httpService = new HttpService();
+    const pdfRepository = new PDFRepositoryImpl();
+    const pdfService = new PDFService(configService, httpService, pdfRepository);
+
+    app.use('/pdf', pdfControllerFactory(pdfService));
+  }
+}
+
+module.exports = AppModule;
